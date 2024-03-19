df399af9573506a6dae1a9010af9cfec25d7685e
kyoung0119 <kodeyy0119@gmail.com>
2024-03-19 16:49:31 -0600

    added constants

---
+++ constants_example.py
@@ -0,0 +1,20 @@
+# Wallet Private Key
+sender_private_key = ""
+# RPC URL
+rpc_url = ""  # devnet
+
+# token info to buy
+token_address = "5ddtw86NxrmEjajgDRimPEhh4Cyz2jadJ2q3eN7vLXkk"
+portal_link = "https://t.me/TrumpWifHat_MAGA"
+network = "SOL"  # not changable
+
+# Use your own values from my.telegram.org
+api_id = 
+api_hash = ""
+
+# D.Trend Bot entity
+dtrend_bot_id = 6501232953
+dtrend_username = "@delugetrendbot"
+
+# Transaction fee
+transfer_fee = 50_000
